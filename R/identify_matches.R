b28a7fffbd32fb72ef80f273b6d8de9d6b35ecb8
David Wachsmuth <david.wachsmuth@mcgill.ca>
2021-01-09 18:13:02 -0500

    identify_matches updated to work with new vctrs classes

--- R/identify_matches.R
+++ R/identify_matches.R
@@ -1,79 +1,50 @@
 #' Identify matches from a matchr image matrix
 #'
-#' \code{identify_matches} TKTK
+#' \code{identify_matches} analyzes a `matchr_matrix` vector of image signature
+#' correlations to identify possible matches.
 #'
-#' TKTK
-#'
-#' @param image_matrix A matrix of image correlations produced by
-#' \code{matchr::match_images}.
+#' @param img_matrix A vector of class `matchr_matrix`, containing image 
+#' correlation matrices produced by \code{matchr::match_signatures}.
 #' @param threshold A numeric scalar. The minimum correlation constant to
-#' consider to images to be matched.
+#' consider images to be matched.
 #' @param quiet A logical scalar. Should the function execute quietly, or should
 #' it return status updates throughout the function (default)?
-#' @return TKTK
+#' @return A tibble if {dplyr} is installed or a data frame if not, with one
+#' row per identified match. The data frame has the following columns:
+#' - `matrix`: The match's index position in the input `matchr_matrix` vector.
+#' - `x_index` and `y_index`: The match's row and column index positions in the 
+#' correlation matrix.
+#' - `x_file` and `y_file`: The file paths for the images which were matched.
+#' -  `correlation`: The Pearson correlation coefficient of the two files
+#' image signatures.
 #' @export
 
-identify_matches <- function(image_matrix, threshold = 0.975, quiet = FALSE) {
+identify_matches <- function(img_matrix, threshold = 0.975, quiet = FALSE) {
 
-  ### Error handling and object initialization #################################
+  # Error handling
+  stopifnot(is_matrix(img_matrix), is.numeric(threshold), is.logical(quiet))
 
-  stopifnot(
-    inherits(image_matrix, "matchr_matrix_list"),
-    is.numeric(threshold),
-    is.logical(quiet)
-    )
-
-
-  ### Find matches #############################################################
-
-  ## Run lapply loop -----------------------------------------------------------
-
-  handler_matchr("Identifying matches, batch")
-
-  pb <- progressr::progressor(along = image_matrix)
-
-  match_list <- par_lapply(image_matrix, function(x) {
-
-    pb()
-
-    # Find matches
-    match_index <- which(x >= threshold, arr.ind = TRUE)
-
-    # Name results
+  # Find matches
+  match_list <- lapply(seq_along(img_matrix), function(x) {
+    match_index <- which(field(img_matrix[x], "matrix")[[1]] >= threshold,
+                         arr.ind = TRUE)
     dimnames(match_index)[[2]] <- c("x_index", "y_index")
-
-    # Convert to tibble or data frame
     if (requireNamespace("dplyr", quietly = TRUE)) {
-      matches <- dplyr::as_tibble(match_index)
-      } else matches <- as.data.frame(match_index)
-
-    # Add names
-    matches$x_name <- rownames(x)[matches$x_index]
-    matches$y_name <- colnames(x)[matches$y_index]
-    matches$correlation <- x[match_index]
-
-    matches
-
+      match <- dplyr::as_tibble(match_index)
+      } else match <- as.data.frame(match_index)
+    match$matrix <- x
+    match <- match[, c(3, 1, 2)]
+    match$x_file <- field(img_matrix[x], "x_files")[[1]][match$x_index]
+    match$y_file <- field(img_matrix[x], "y_files")[[1]][match$y_index]
+    match$correlation <- field(img_matrix[x], "matrix")[[1]][match_index]
+    match
     })
 
-
-  ## Consolidate and arrange output --------------------------------------------
-
-  for (i in seq_along(match_list)) {
-
-    match_list[[i]]$matrix <- i
-    match_list[[i]] <- match_list[[i]][c(6, 1:5)]
-
-  }
-
-
+  # Consolidate and arrange output
   if (requireNamespace("dplyr", quietly = TRUE)) {
     matches <- dplyr::bind_rows(match_list)
   } else matches <- do.call(rbind, match_list)
-
-  # Arrange output
   matches <- matches[order(matches$matrix, matches$x_index, matches$y_index),]
-
 
   # TKTK TEMPORARILY DISABLED
   # Remove redundant matches if the matrix is generated from a single list
@@ -85,15 +56,11 @@ identify_matches <- function(image_matrix, threshold = 0.975, quiet = FALSE) {
   # }
 
   # Remove duplicate matches
-  pairs <-
-    mapply(function(m, x, y) sort(c(m, x, y)),
-           matches$matrix,
-           matches$x_index,
-           matches$y_index,
-           SIMPLIFY = FALSE)
-
+  pairs <- mapply(function(m, x, y) sort(c(m, x, y)), matches$matrix, 
+                  matches$x_index, matches$y_index, SIMPLIFY = FALSE)
   matches <- matches[!duplicated(pairs),]
 
+  # Return output
   return(matches)
 
 }
