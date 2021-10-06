c124a052847c0f77230e348977a4e5abdc28381c
David Wachsmuth <david.wachsmuth@mcgill.ca>
2021-10-06 14:01:42 -0400

    identify_matches updated for matchr 0.3

--- R/identify_matches.R
+++ R/identify_matches.R
@@ -1,26 +1,21 @@
 #' Identify matches from a matchr image matrix
 #'
 #' \code{identify_matches} analyzes a `matchr_matrix` vector of image signature
-#' correlations to identify possible matches. By default it sets a low
+#' Hamming distances to identify possible matches. By default it sets a low
 #' threshold for identifying matches, with the assumption that the results will
 #'be refined using \code{\link{confirm_matches}}, either within the function if
 #'`confirm = TRUE`, or subsequently with a separate function call.
 #'
 #' @param x A vector of class `matchr_sig`, containing image signatures produced
 #' by \code{\link{create_signature}}, or `matchr_matrix`, containing image 
-#' correlation matrices produced by \code{\link{match_signatures}}.
+#' signature Hamming distance matrices produced by 
+#' \code{\link{match_signatures}}.
 #' @param y A vector of class `matchr_sig`, containing image signatures produced
 #' by \code{\link{create_signature}}. If x is not a `matchr_sig` vector, this
 #' argument is ignored, and will trigger a warning if a non-NULL value is
 #' supplied.
-#' @param threshold A numeric scalar. The minimum correlation constant to
-#' consider images to be matched.
-#' @param confirm A logical scalar. Should the function run 
-#' \code{\link{confirm_matches}} on the results before returning them (default)?
-#' If TRUE, \code{\link{confirm_matches}} will be called with default 
-#' parameters, i.e. under the assumption that the original signature matching
-#' was done using greyscale signatures, and the confirmation should be carried
-#' out with colour signatures with the default tolerance thresholds.
+#' @param threshold A numeric scalar. The maximum Hamming distance to consider 
+#' images to be matched.
 #' @param quiet A logical scalar. Should the function execute quietly, or should
 #' it return status updates throughout the function (default)?
 #' @param ... Additional arguments passed to methods.
@@ -30,10 +25,15 @@
 #' Each element is a length-three integer vector giving, respectively, the
 #' matrix, row index position, and column index position of the match.
 #' - `x_path` and `y_path`: The file paths for the images which were matched.
-#' - `correlation`: The Pearson correlation coefficient of the two files'
-#' image signatures.
-#' - `match` (if `confirm = TRUE`): A character vector indicating match status.
-#' (See \code{\link{confirm_matches}} for details.)
+#' - `distance`: An integer vector giving the Hamming distance between the two 
+#' files' image signatures. (The lower the distance, the more perceptually 
+#' similar the images are.)
+#' - `dd`: An integer vector giving the product of the Hamming distances between
+#' the two files' primary and secondary image signatures. The secondary image
+#' signature is less reliable than the primary one, but can be a helpful
+#' additional data point in assessing potential matches with borderline 
+#' primary-signature Hamming distances. In general, the lower the `dd` value, 
+#' the higher the chance that two images are the same.
 #' @examples
 #' \dontrun{
 #' # Setup
@@ -48,8 +48,7 @@
 #' }
 #' @export
 
-identify_matches <- function(x, y = NULL, threshold = 0.975, confirm = TRUE, 
-                             quiet = FALSE, ...) {
+identify_matches <- function(x, y = NULL, threshold = 12, quiet = FALSE, ...) {
   
   UseMethod("identify_matches")
   
@@ -61,16 +60,20 @@ identify_matches <- function(x, y = NULL, threshold = 0.975, confirm = TRUE,
 #' @method identify_matches matchr_matrix
 #' @export
 
-identify_matches.matchr_matrix <- function(x, y = NULL, threshold = 0.975, 
-                                           confirm = TRUE, quiet = FALSE, ...) {
-
+identify_matches.matchr_matrix <- function(x, y = NULL, threshold = 200, 
+                                           quiet = FALSE, ...) {
+  
   # Error handling
-  stopifnot(is_matrix(x), is.numeric(threshold), is.logical(confirm), 
-            is.logical(quiet))
-
+  stopifnot(is_matrix(x), is.numeric(threshold), is.logical(quiet))
+  if (!missing(y)) {
+    stop("`identify_matches` cannot take a `y` argument when the `x` argument ",
+         "is a matchr_matrix.")
+  }
+  
   # Initialize progress reporting
   handler_matchr("Identifying matches, batch")
-  prog_bar <- as.logical(as.numeric(!quiet) * progressr::handlers(global = NA))
+  prog_bar <- as.logical(
+    as.numeric(!quiet) * progressr::handlers(global = NA) * check_env())
   pb <- progressr::progressor(steps = sum(sapply(get_array(x), vec_size)), 
                               enable = prog_bar)
   
@@ -78,39 +81,40 @@ identify_matches.matchr_matrix <- function(x, y = NULL, threshold = 0.975,
   match_list <- lapply(seq_along(x), identify_matches_internal, x, pb, 
                        threshold)
   
-  # Finish output
+  # Finish output and return
   matches <- identify_matches_finish(match_list)
-
-  # Return output
-  if (confirm) matches <- confirm_matches(matches)
   return(matches)
-
+  
 }
 
 # ------------------------------------------------------------------------------
 
 #' @rdname identify_matches
 #' @method identify_matches matchr_signature
-#' @param method,compare_ar,stretch,mem_scale Arguments passed to 
+#' @param distance,compare_ar,stretch,mem_scale Arguments passed to 
 #' \code{\link{match_signatures}}.
 #' @export
 
 identify_matches.matchr_signature <- function(
-  x, y = NULL, threshold = 0.975, confirm = TRUE, quiet = FALSE, 
-  method = "grey", compare_ar = TRUE, stretch = 1.2, mem_scale = 0.2, ...) {
+  x, y = NULL, threshold = 200, quiet = FALSE, distance = ~ nearest * bilinear, 
+  compare_ar = TRUE, stretch = 1.2, mem_scale = 0.2, ...) {
   
   # Error handling and object initialization
-  stopifnot(is_signature(x), is.logical(c(compare_ar, quiet)),
-            method %in% c("grey", "gray", "colour", "color", "rgb", "RGB", 
-                          "both"))
+  stopifnot(is_signature(x), (is.language(distance) | is.character(distance)),
+            is.logical(c(compare_ar, quiet)))
   if (missing(y)) y <- x else stopifnot(is_signature(y))
+  stopifnot(
+    "`distance` must contain one or both of `nearest` and `bilinear" = 
+      sum(c("nearest", "bilinear") %in% as.character(distance[[2]])) > 0)
   par_check <- TRUE
   
   # Prepare objects for processing
-  output <- suppressWarnings(match_signatures_prep(
-    x, y, method, compare_ar, stretch, mem_scale, mem_override = TRUE))
+  output <- suppressWarnings(
+    match_signatures_prep(x, y, compare_ar, stretch, mem_scale, mem_override))
   x <- output[[1]]
   y <- output[[2]]
+  x_na <- output[[3]]
+  y_na <- output[[4]]
   x_list <- output[[5]]
   y_list <- output[[6]]
   x_sig <- output[[7]]
@@ -120,39 +124,39 @@ identify_matches.matchr_signature <- function(
   # Initialize progress reporting
   handler_matchr("Identifying matches, row")
   prog_bar <- as.logical((vec_size(x) >= 5000) * as.numeric(!quiet) *
-                           progressr::handlers(global = NA))
+                           progressr::handlers(global = NA) * check_env())
   pb <- progressr::progressor(steps = vec_size(x), enable = prog_bar)
   
-  # Calculate correlation matrices
-  blas <- getOption("matchr.blas", as.logical(Sys.getenv("MATCHR_BLAS", TRUE)))
-  if (!blas && !quiet) message("Function operating in forced parallel mode.")
+  # Loop over signatures one-by-one to reduce memory footprint
   result <- vector("list", length(x_list))
   for (i in seq_along(x_list)) {
-    result[[i]] <- match_signatures_internal(x_list[[i]], y_list[[i]], blas)
     
+    # Create distance matrix
+    result[[i]] <- match_signatures_internal(x_list[[i]], y_list[[i]], distance)
+    
+    # Create intermediate matchr_matrix
     result[[i]] <- new_matrix(
       array = result[i],
       x_ar = list(get_ratios(x_list[[i]])),
       y_ar = list(get_ratios(y_list[[i]])),
       x_sig = x_sig[i],
       y_sig = y_sig[i],
-      x_total = vec_size(x_list[[i]]),
-      y_total = vec_size(y_list[[i]]),
-      x_na = character(),
-      y_na = character()
+      x_total = length(unique(c(get_path(x), get_path(x_na)))),
+      y_total = length(unique(c(get_path(y), get_path(y_na)))),
+      x_na = get_path(x_na),
+      y_na = get_path(y_na),
+      formula = as.character(distance)[[2]]
     )
     
-    result[[i]] <- 
-      identify_matches_internal(i, result, function() NULL, threshold)
+    # Find matches from intermediate matrix
+    result[[i]] <- identify_matches_internal(1, result[[i]], pb, threshold)
+    result[[i]]$matrix <- i
     
     pb(amount = sum(sapply(x_list[[i]], vec_size)))
   }
-
-  # Finish output
-  matches <- identify_matches_finish(result)
   
-  # Return output
-  if (confirm) matches <- confirm_matches(matches)
+  # Finish and return output
+  matches <- identify_matches_finish(result)
   return(matches)
 }
 
@@ -160,7 +164,8 @@ identify_matches.matchr_signature <- function(
 
 identify_matches_internal <- function(n, x, pb, threshold) {
   
-  match_index <- which(get_array(x[[n]])[[1]] >= threshold, arr.ind = TRUE)
+  match_index <- which(get_array(x)[[n]] <= threshold, arr.ind = TRUE)
+  
   dimnames(match_index)[[2]] <- c("x_index", "y_index")
   
   if (requireNamespace("dplyr", quietly = TRUE)) {
@@ -172,7 +177,8 @@ identify_matches_internal <- function(n, x, pb, threshold) {
   
   match$x_sig <- get_x_sig(x[[n]])[[1]][match$x_index]
   match$y_sig <- get_y_sig(x[[n]])[[1]][match$y_index]
-  match$correlation <- get_array(x[[n]])[[1]][match_index]
+  match$distance <- get_array(x[[n]])[[1]][match_index]
+  match$distance <- as.integer(match$distance)
   
   match
   
@@ -189,11 +195,11 @@ identify_matches_finish <- function(match_list) {
     x_sig <- do.call(c, lapply(match_list, function(x) x$x_sig))
     y_sig <- do.call(c, lapply(match_list, function(x) x$y_sig))
     matches <- do.call(rbind, lapply(match_list, function(x) 
-      x[c("matrix", "x_index", "y_index", "correlation")]))
+      x[c("matrix", "x_index", "y_index", "distance")]))
     matches$x_sig <- x_sig
     matches$y_sig <- y_sig
     matches <- matches[c("matrix", "x_index", "y_index", "x_sig", "y_sig", 
-                         "correlation")]
+                         "distance")]
   }
   matches <- matches[order(matches$matrix, matches$x_index, matches$y_index),]
   
@@ -206,11 +212,9 @@ identify_matches_finish <- function(match_list) {
   matches <- matches[!duplicated(matches$hash),]
   matches$hash <- NULL
   
-  # Merge index fields
+  # Merge index fields and return output
   matches$index <- mapply(c, matches$matrix, matches$x_index, matches$y_index, 
                           SIMPLIFY = FALSE)
-  matches <- matches[c("index", "x_sig", "y_sig", "correlation")]
-  
-  # Return output
-  matches
+  matches <- matches[c("index", "x_sig", "y_sig", "distance")]
+  return(matches)
 }
--- tests/testthat/test-identify_matches.R
+++ tests/testthat/test-identify_matches.R
@@ -3,18 +3,23 @@
 test_that("the function works", {
   expect_equal(vec_size(test_identify), 6)
   expect_equal(vec_size(identify_matches(test_long_sig)), 6)
+  expect_equal(vec_size(identify_matches(test_long_sig, test_long_sig)), 6)
+})
+
+test_that("y is NULL if x is matchr_matrix", {
+  expect_error(identify_matches(test_match, y = test_match), "a `y` argument")
 })
 
 test_that("tibble integration works", {
   expect_s3_class(test_identify, "tbl_df")
-  
+
   old_fn <- base::requireNamespace
   requireNamespace1 <- function(...) FALSE
   unlockBinding("requireNamespace", as.environment("package:base"))
   assign("requireNamespace", requireNamespace1, "package:base")
-  
+
   expect(!inherits(identify_matches(test_match), "tbl_df"), "tbl_df class")
   assign("requireNamespace", old_fn, "package:base")
   lockBinding("requireNamespace", as.environment("package:base"))
-  
+
 })
